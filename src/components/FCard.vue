84c6cd329c457c83dd3d46e400814e35ab10d8f3
klren <kuanglong0312@sina.com>
2019-07-11 23:21:06 +0800

    首页卡片

--- src/components/FCard.vue
+++ src/components/FCard.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="f-card">
-    <div class="title">拒贷天眼</div>
-    <div class="tip">组合查询，更全，更准，更优惠</div>
-    <div class="tip">多维度网黑查询，多头借贷检测</div>
+    <div class="title">{{category.category_name}}</div>
+    <div class="tip" v-for="(v, i) in category.tags" :key="i">{{v}}</div>
     <div class="card-btn">
       <md-button class="start-btn" type="primary" inline>开始检测</md-button>
+      <a class="example-link" href="javascript:;">样例</a>
+      <span class="price-text">{{category.price}} 元/次</span>
     </div>
     <div class="card-right-img">
       <span class="space"></span>
@@ -13,56 +14,76 @@
   </div>
 </template>
 <script>
-import { Button } from 'mand-mobile'
-export default {
-  name: 'FCard',
-  components: {
-    [Button.name]: Button
+  import {
+    Button
+  } from 'mand-mobile'
+  export default {
+    name: 'FCard',
+    components: {
+      [Button.name]: Button
+    },
+    props: {
+      category: {
+        type: Object,
+        default: {
+          category_name: '拒贷天眼',
+          tags: ['组合查询，更全，更准，更优惠', '多维度网黑查询，多头借贷检测'],
+          price: '39.90'
+        }
+      }
+    }
   }
-}
 </script>
 <style lang="stylus" scoped>
-.f-card
-  padding 40px 40px 0
-  position relative
-  margin-bottom 40px
-  box-sizing border-box
-  border-radius 6px
-  box-shadow 0 0 0.25rem 0.15rem rgba(0,96,255,.08)
-  background #ffffff
-  z-index 1
-  .title
-    font-size .32rem
-    line-height 1em
-    text-overflow ellipsis
-    white-space nowrap
-    color #4c546a
-  .tip
-    margin-top .3rem
-    font-size .24rem
-    line-height 1em
-    color #999
-  .card-btn
-    padding .4rem 0
-  .start-btn
-    width 2rem
-    height .48rem
-    background #596dff
-    margin-right .3rem
-    box-shadow 0 0 0.1rem 0.08rem rgba(89,109,255,.15)
-    font-size .24rem
-  .card-right-img
-    position absolute
-    top 0
-    right .4rem
-    height 100%
-    line-height 1em
-    z-index -1
-    .space
-      display inline-block
-      height 100%
-      vertical-align middle
-    .card-img
-      width 2.2rem
-      vertical-align middle
+  .f-card
+    padding 40px 40px 0
+    position relative
+    margin-bottom 40px
+    box-sizing border-box
+    border-radius 6px
+    box-shadow 0 0 0.25rem 0.15rem rgba(0, 96, 255, .08)
+    background #ffffff
+    z-index 1
+    .title
+      font-size .32rem
+      line-height 1em
+      text-overflow ellipsis
+      white-space nowrap
+      color #4c546a
+    .tip
+      margin-top .3rem
+      font-size .24rem
+      line-height 1em
+      color #999 
+    .card-btn 
+      padding .4rem 0
+      line-height 0
+      .start-btn 
+        display inline-block 
+        width 2rem 
+        height .48rem 
+        background #596dff 
+        margin-right .3rem 
+        box-shadow 0 0 0.1rem 0.08rem rgba(89, 109, 255, .15) 
+        font-size .24rem 
+    .card-right-img 
+      position absolute 
+      top 0 
+      right .4rem 
+      height 100% 
+      line-height 1em 
+      z-index -1 
+    .space 
+      display inline-block 
+      height 100% 
+      vertical-align middle 
+    .card-img 
+      width 2.2rem 
+      vertical-align middle 
+    .example-link
+      margin-right 0.1rem
+      text-decoration underline 
+      color rgb(102, 127, 254) 
+    .price-text
+      color #999
 </style>
--- src/views/homePage/index.vue
+++ src/views/homePage/index.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="header"></div>
     <div class="feature-container">
-      <f-card></f-card>
+      <f-card v-for="(v, i) in category" :key="i" :category="v"></f-card>
     </div>
   </div>
 </template>
@@ -12,6 +12,50 @@ export default {
   name: 'HomePage',
   components: {
     FCard
+  },
+  data () {
+    return {
+      category: [{
+        "category_id": 9,
+        "category_name": "拒贷天眼",
+        "logo": "http:\/\/www.sukaquan.net\/uploads\/images\/20190507\/9003d62018ef377d082487f78b837b78.png",
+        "description": "",
+        "tips": "风控必查、优化风险项",
+        "tags": ["组合查询，更全，更准，更优惠", "多维度网黑查询，多头借贷检测，"],
+        "price": "39.90",
+        "code": "risk_5",
+        "top": 1,
+        "hot": 1,
+        "discounts": "2.00",
+        "detail_bg": "http:\/\/www.sukaquan.net\/uploads\/images\/20190511\/d19ad025cae4133706717aa5afeefee2.jpg"
+      }, {
+        "category_id": 1,
+        "category_name": "贷前综合风险核查",
+        "logo": "http:\/\/www.sukaquan.net\/uploads\/images\/20190507\/7c5976beed7de392b354fde1e38dc4a7.png",
+        "description": "",
+        "tips": "风控必查、优化风险项",
+        "tags": ["核心风控项", "贷前必查"],
+        "price": "29.90",
+        "code": "risk_1",
+        "top": 1,
+        "hot": 1,
+        "discounts": "2.00",
+        "detail_bg": "http:\/\/www.sukaquan.net\/uploads\/images\/20190511\/32138721d7953d23317cce0f31415402.jpg"
+      }, {
+        "category_id": 7,
+        "category_name": "黑名单检测",
+        "logo": "http:\/\/www.sukaquan.net\/uploads\/images\/20190507\/80c768ab13bd31ab17015eabcc8187a3.png",
+        "description": "",
+        "tips": "风控必查、优化风险项",
+        "tags": ["网贷被拒？", "一定是上黑名单"],
+        "price": "26.60",
+        "code": "risk_3",
+        "top": 1,
+        "hot": 1,
+        "discounts": "2.00",
+        "detail_bg": "http:\/\/www.sukaquan.net\/uploads\/images\/20190511\/6ad4375e379828c8e2ff993f6a62f0c0.jpg"
+      }]
+    }
   }
 }
 </script>
